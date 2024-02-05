66f1cc386a0c759eff9ed207b1226a54a1aa1b8e
no0ne <sumit@n4ksum>
2024-02-05 20:48:06 +0530

    uploads

---
+++ yolov8_basic.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+import numpy
+
+
+model = YOLO("weights/best.pt", "v8")  
+
+# predict on an image
+detection_output = model.predict(source="uploads/images/image1.jpg", conf=0.25, save=True) 
+
+# Display tensor array
+print(detection_output)
+
+# Display numpy array
+print(detection_output[0].numpy())
